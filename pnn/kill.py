0ea722db8a28b6b5035fd0c315c4aa17c35789b4
KevinKune <kevinqu16@gmail.com>
2018-03-16 10:58:54 +0800

    minor

---
+++ pnn/kill.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import subprocess
+
+out_bytes = subprocess.check_output(['ps', '-aux'])
+out_text = out_bytes.decode('utf-8')
+out_text = out_text.strip().split('\n')
+for line in out_text:
+	if 'tf_main_multi_gpu.py' in line:
+		print line
+		id = line.strip().split()[1]
+		subprocess.call('kill %s' % id, shell=True)
